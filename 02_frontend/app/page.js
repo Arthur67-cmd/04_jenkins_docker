d3ac5873cfa7ce529032b55034eb93249a4f59cb
Arthur67-cmd <rthar.s67@rsu.ac.th>
2025-11-04 19:24:01 +0700

    Arthur

--- 02_frontend/app/page.js
+++ 02_frontend/app/page.js
@@ -44,6 +44,7 @@ export default function Page() {
   return (
     <main className="container">
       <header className="header">
+        <h1 className="title">Arthur 6703450</h1>
         <h1 className="title">Attractions</h1>
         <p className="subtitle">Discover points of interest nearby</p>
       </header>
